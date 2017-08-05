554d2f1db49194abbea028d3d71bc5d31cf4e8bb
KonstantinZoerner <drei3.zett@gmail.com>
2017-08-05 11:45:50 +0200

    Neue Orientierung
    Orientierung und Funktionen

--- me_lcd_breakout.ino
+++ me_lcd_breakout.ino
@@ -6,12 +6,27 @@ MeSerial mySerial(PORT_5);
 #define ANZEIGE_BREITE = 240
 #define ANZEIGE_HOEHE  = 320
 
-void setup() {
-  // put your setup code here, to run once:
+void male_rechteck (int x, int y, int breite, int hoehe, int farbe) {
+      mySerial.println("BOXF(x,y,y + breite,y + hoehe, farbe);");  
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void rand_malen(){
+  male_rechteck(10, 10, 20, 30, 2);
+}
 
+void setup() {
+  mySerial.begin(9600);
+  mySerial.print("DR1;");    // the screen displays in upright way
+  mySerial.println("CLS(0);");
+  mySerial.println("");
+  mySerial.println("DS24(64,104,'Hallo!',5);"); 
+
+
+  delay(1000); 
+}
+
+
+void loop() {
+  rand_malen();
 }
