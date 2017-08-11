82eca237db3b4fbf510732ac0a7b9f9758d6e96e
KonstantinZoerner <drei3.zett@gmail.com>
2017-08-11 17:15:40 +0200

    Konstanten ausgelagert

--- src/me_lcd_breakout.ino
+++ src/me_lcd_breakout.ino
@@ -2,41 +2,11 @@
 
 #include <MeAuriga.h>
 #include <SoftwareSerial.h>
+#include "konstanten.h"
 
 MeSerial serial(PORT_5);
 MeJoystick joystick(6);
 
-#define ANZEIGE_BREITE 240
-#define ANZEIGE_HOEHE  320
-#define RAND_BREITE 2
-
-#define BALL_GROESSE 10
-#define BALL_RADIUS  5
-#define BALL_FARBE   15
-
-#define SCHLAEGER_HOEHE 7
-#define SCHLAEGER_BREITE 50
-#define SCHLAEGER_POS_Y 270
-#define SCHLAEGER_FARBE 4
-
-#define STEIN_BREITE 25
-#define STEIN_HOEHE 10
-#define STEIN_ABSTAND 3
-
-
-#define STEINE_PRO_REIHE    8
-#define ANZAHL_STEIN_REIHEN 6
-
-// Postionen (Pixel), ab denen die Mauer gemalt wird
-//
-#define STEINE_START_X 9
-#define STEINE_START_Y 40
-
-// Postionen (Pixel), fuer die Anzeige
-#define ANZEIGE_START_X 7
-#define ANZEIGE_START_Y 307
-
-#define MAX_BAELLE 3
 
 
 int ball_pos_x;
