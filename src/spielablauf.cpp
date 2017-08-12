a844f10752119cb9953488f8044b366000f868cb
StefanZ <stefan.zoerner@embarc.de>
2017-08-12 17:28:43 +0200

    Spielablauf eingefuehrt;

---
+++ src/spielablauf.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+
+#include "konstanten.h"
+
+
+int anzahl_punkte   = 0;
+int anzahl_baelle   = 0;
+int aktueller_level = 0;
+int anzahl_steine   = 0;
+
+
+byte stein_liste[ANZAHL_STEIN_REIHEN][STEINE_PRO_REIHE];
+
+
+void steine_initialisieren(int level) {
+
+  anzahl_steine = 0;
+
+  for (int i=0; i<ANZAHL_STEIN_REIHEN; ++i) {
+    for (int j = 0; j < STEINE_PRO_REIHE; ++j) {
+      stein_liste[i][j] = 1;
+      anzahl_steine++;
+    }
+  }
+}
+
+
+void neues_spiel_beginnen() {
+  anzahl_baelle   = 3;
+  aktueller_level = 1;
+  anzahl_punkte   = 0;
+
+  steine_initialisieren(1);
+}
+
+void ball_verlieren() {
+  anzahl_baelle--;
+}
+
+void neuer_level() {
+  aktueller_level++;
+  steine_initialisieren(aktueller_level);
+}
+
+bool ist_spiel_zuende() {
+  return anzahl_baelle == 0;
+}
