b1f127a2ce66b513ff293122cc400ba5fcc567a9
StefanZ <stefan.zoerner@embarc.de>
2017-08-12 17:38:09 +0200

    Level unterschiedlich

--- src/spielablauf.cpp
+++ src/spielablauf.cpp
@@ -13,15 +13,28 @@ byte stein_liste[ANZAHL_STEIN_REIHEN][STEINE_PRO_REIHE];
 
 
 void steine_initialisieren(int level) {
-
   anzahl_steine = 0;
 
-  for (int i=0; i<ANZAHL_STEIN_REIHEN; ++i) {
-    for (int j = 0; j < STEINE_PRO_REIHE; ++j) {
-      stein_liste[i][j] = 1;
-      anzahl_steine++;
-    }
+  switch (level) {
+
+    case 1:
+      for (int i=0; i<ANZAHL_STEIN_REIHEN; ++i) {
+        for (int j = 0; j < STEINE_PRO_REIHE; ++j) {
+          stein_liste[i][j] = 1 + i;
+          anzahl_steine++;
+        }
+      }
+      break;
+
+    default:
+      for (int i=0; i<ANZAHL_STEIN_REIHEN; ++i) {
+        for (int j = 0; j < STEINE_PRO_REIHE; ++j) {
+          stein_liste[i][j] = 1;
+          anzahl_steine++;
+        }
+      }
   }
+
 }
 
 
@@ -33,14 +46,17 @@ void neues_spiel_beginnen() {
   steine_initialisieren(1);
 }
 
+
 void ball_verlieren() {
   anzahl_baelle--;
 }
+
 
 void neuer_level() {
   aktueller_level++;
   steine_initialisieren(aktueller_level);
 }
+
 
 bool ist_spiel_zuende() {
   return anzahl_baelle == 0;
