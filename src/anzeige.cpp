d131367ae4ca6c447e1ff02734c46950d8d45f39
StefanZ <stefan.zoerner@embarc.de>
2017-08-13 10:12:41 +0200

    Anzeige-Methoden aufgeraeumt.

--- src/anzeige.cpp
+++ src/anzeige.cpp
@@ -5,7 +5,7 @@
 
 #include "konstanten.h"
 
-MeSerial serial(PORT_5);
+MeSerial serial(PORT_5); // LCD-Anzeige an Port 5 angeschlossen
 char buffer[80];
 
 void anzeige_initialisieren() {
@@ -16,10 +16,12 @@ void anzeige_initialisieren() {
   serial.println("DS24(64,104,' ',5);");
 }
 
+
 void male_rechteck (int x, int y, int breite, int hoehe, int farbe) {
   sprintf(buffer, "BOXF(%d,%d,%d,%d,%d);", x, y, x + breite, y + hoehe, farbe);
   serial.println(buffer);
 }
+
 
 void buffer_schreiben() {
   serial.println(buffer);
@@ -27,37 +29,29 @@ void buffer_schreiben() {
 
 
 void male_punkte_leiste() {
-
-  // Anzahl Punkte und Level
-  sprintf(buffer, "DS16(%d,%d,'Pts:%5d  Lvl:%2d  Bls: ',%d);", ANZEIGE_START_X, ANZEIGE_START_Y, 0, 0, SCHLAEGER_FARBE);
-  buffer_schreiben();
-
-/*
-    // Baelle
-    for (int i = 0; i < MAX_BAELLE; ++i)
-    {
-      sprintf(buffer, "CIRF(%d,%d,%d,%d);", ANZEIGE_START_X + 190 + i * (BALL_GROESSE+3), ANZEIGE_START_Y + BALL_RADIUS + 2, BALL_RADIUS, SCHLAEGER_FARBE);
-      buffer_schreiben();
-    }
-    */
+  sprintf(buffer, "DS16(%d,%d,'Pts:%5d  Lvl:%2d  Bls: ',%d);",
+      ANZEIGE_START_X, ANZEIGE_START_Y, 0, 0, SCHLAEGER_FARBE);
+  serial.println(buffer);
 }
 
 
 void male_punktstand(int punkte) {
-  sprintf(buffer, "DS16(%d,%d,'%5d',%d);", ANZEIGE_START_X + 32, ANZEIGE_START_Y, punkte, SCHLAEGER_FARBE);
+  sprintf(buffer, "DS16(%d,%d,'%5d',%d);",
+      ANZEIGE_START_X + 32, ANZEIGE_START_Y, punkte, SCHLAEGER_FARBE);
   serial.println(buffer);
 }
 
 
 void male_level_nr(int lvl) {
-  sprintf(buffer, "DS16(%d,%d,'%2d',%d);", ANZEIGE_START_X + 120, ANZEIGE_START_Y, lvl, SCHLAEGER_FARBE);
+  sprintf(buffer, "DS16(%d,%d,'%2d',%d);",
+      ANZEIGE_START_X + 120, ANZEIGE_START_Y, lvl, SCHLAEGER_FARBE);
   serial.println(buffer);
 }
 
+
 void male_anzahl_baelle(int anzahl) {
-  // Baelle
-  for (int i = 0; i < MAX_BAELLE; ++i)
-  {
+  for (int i = 0; i < MAX_BAELLE; ++i) {
+    // Vorhandener Ball ist ausgefuellt
     if (anzahl > i) {
       sprintf(buffer, "CIRF(%d,%d,%d,%d);",
         ANZEIGE_START_X + 190 + i * (BALL_GROESSE+3), ANZEIGE_START_Y + BALL_RADIUS + 2, BALL_RADIUS, SCHLAEGER_FARBE);
@@ -65,7 +59,7 @@ void male_anzahl_baelle(int anzahl) {
       sprintf(buffer, "CIR(%d,%d,%d,%d);",
         ANZEIGE_START_X + 190 + i * (BALL_GROESSE+3), ANZEIGE_START_Y + BALL_RADIUS + 2, BALL_RADIUS, SCHLAEGER_FARBE);
     }
-    buffer_schreiben();
+    serial.println(buffer);
   }
 }
 
