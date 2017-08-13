363516a47facd02bc0545362800481c7ccbb607c
StefanZ <stefan.zoerner@embarc.de>
2017-08-13 12:10:52 +0200

    Neue Header-Dateien eingefuehrt

---
+++ src/ball.h
@@ -0,0 +1,7 @@
+extern void neuen_ball_starten();
+extern void berechneBallPosition();
+extern void pruefeBallGegenMauer();
+
+extern void maleBallNeu();
+extern void maleSchlaegerNeu();
+extern void steineMalen();
---
+++ src/schlaeger.h
@@ -0,0 +1,4 @@
+#include "Arduino.h"
+
+extern bool berechneSchlaegerPosition();
+extern int schlaeger_x;
