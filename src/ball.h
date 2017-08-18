c258e4deb7b93cdbcd9a57c70178e2252f7ed1ab
StefanZ <stefan.zoerner@embarc.de>
2017-08-18 10:28:33 +0200

    maleSchlaegerNeu umgezogen

--- src/ball.h
+++ src/ball.h
@@ -3,7 +3,6 @@ extern void berechneBallPosition();
 extern void pruefeBallGegenMauer();
 
 extern void maleBallNeu();
-extern void maleSchlaegerNeu();
 extern void steineMalen();
 extern bool ball_raus_ueberpruefen();
 extern void ball_platzen();
--- src/schlaeger.h
+++ src/schlaeger.h
@@ -1,5 +1,7 @@
 #include "Arduino.h"
 
 extern bool berechneSchlaegerPosition();
+extern void maleSchlaegerNeu();
+
 extern int schlaeger_x;
 extern int schlaeger_x_alt;
